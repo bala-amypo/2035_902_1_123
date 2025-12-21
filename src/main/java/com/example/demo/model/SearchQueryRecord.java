4304aa95f2ad3031b001c79fd45b2e378de59a59
bala-amypo <bala@amypo.in>
2025-12-21 16:02:34 +0000

    User commit

--- src/main/java/com/example/demo/model/SearchQueryRecord.java
+++ src/main/java/com/example/demo/model/SearchQueryRecord.java
@@ -1,12 +1,9 @@
 package com.example.demo.model;
-
 import jakarta.persistence.*;
 import java.sql.Timestamp;
-
 @Entity
 @Table(name = "search_query_records")
 public class SearchQueryRecord {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
