2fa90f49f816f8b3b4ea464b243812b262ed33ed
bala-amypo <bala@amypo.in>
2025-12-23 05:01:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
+    List<Employee> findByActiveTrue();
 }
--- src/main/java/com/example/demo/repository/SkillRepository.java
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.model.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+    Optional<Skill> findByName(String name);
+
+    List<Skill> findByActiveTrue();
 }
