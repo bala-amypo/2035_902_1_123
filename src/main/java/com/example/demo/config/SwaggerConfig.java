390da0dbcc8d8efc729fdf7aa583a237ba21e5cf
bala-amypo <bala@amypo.in>
2025-12-26 06:10:38 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,49 +1,21 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.servers.Server;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import java.util.List;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
-// @Configuration
-// public class SwaggerConfig {
+@Configuration
+public class SwaggerConfig {
 
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//                 // You need to change the port as per your server
-//                 .servers(List.of(
-//                         new Server().url("https://9050.408procr.amypo.ai/")
-//                 ));
-//         }
-// }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9050.408procr.amypo.ai/")
+                ));
+        }
+}
 
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.Components;
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class SwaggerConfig {
-
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//             .info(new Info()
-//                 .title("Employee Skills Matrix API")
-//                 .version("1.0")
-//                 .description("API documentation for the Employee Skills Matrix system"))
-//             .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // Enable global security requirement
-//             .components(new Components()
-//                 .addSecuritySchemes("bearerAuth", new SecurityScheme()
-//                     .type(SecurityScheme.Type.HTTP)
-//                     .scheme("bearer")
-//                     .bearerFormat("JWT")));
-//     }
-// }
