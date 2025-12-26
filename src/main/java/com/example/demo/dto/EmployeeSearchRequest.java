faaf075007bca8128db7836da7015f2e8a42d11a
bala-amypo <bala@amypo.in>
2025-12-26 05:04:38 +0000

    User commit

--- src/main/java/com/example/demo/dto/EmployeeSearchRequest.java
+++ src/main/java/com/example/demo/dto/EmployeeSearchRequest.java
@@ -3,8 +3,7 @@ package com.example.demo.dto;
 import java.util.List;
 
 public class EmployeeSearchRequest {
-    private List<String> skills; // Required list of skill names
-
+    private List<String> skills; 
     public List<String> getSkills() { return skills; }
     public void setSkills(List<String> skills) { this.skills = skills; }
 }
