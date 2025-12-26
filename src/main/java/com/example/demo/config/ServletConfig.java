00350055174ee410e8b79dbb450f048f20da6586
bala-amypo <bala@amypo.in>
2025-12-26 08:58:10 +0000

    User commit

--- src/main/java/com/example/demo/config/ServletConfig.java
+++ src/main/java/com/example/demo/config/ServletConfig.java
@@ -1,39 +1,3 @@
-// // package com.example.demo.config;
-
-// // import org.springframework.context.annotation.Configuration;
-// // import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// // import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// // @Configuration
-// // public class ServletConfig implements WebMvcConfigurer {
-
-// //     @Override
-// //     public void addCorsMappings(CorsRegistry registry) {
-// //         registry.addMapping("/**")
-// //                 .allowedOrigins("*") // Allows all origins to prevent "Failed to fetch"
-// //                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-// //                 .allowedHeaders("*")
-// //                 .allowCredentials(false);
-// //     }
-// // }
-// package com.example.demo.config;
-
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// public class ServletConfig implements WebMvcConfigurer {
-
-//     @Override
-// public void addCorsMappings(CorsRegistry registry) {
-//     registry.addMapping("/**")
-//             .allowedOrigins("*") // Allows the Swagger UI to communicate
-//             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//             .allowedHeaders("*")
-//             .allowCredentials(false);
-// }
-// }
 
 package com.example.demo.config;
 
