08eb1f6d052595d669f2568ddef9811d82b4b0b4
bala-amypo <bala@amypo.in>
2025-12-28 04:50:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillCategoryRepository.java
+++ src/main/java/com/example/demo/repository/SkillCategoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.SearchQueryRecord;
+import com.example.demo.model.SkillCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
-public interface SearchQueryRecordRepository extends JpaRepository<SearchQueryRecord, Long> {
-    List<SearchQueryRecord> findBySearcherId(Long searcherId);
+@Repository
+public interface SkillCategoryRepository extends JpaRepository<SkillCategory, Long> {
+     Optional<SkillCategory> findByCategoryName(String categoryName);
 }
